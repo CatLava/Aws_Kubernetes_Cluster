a597e572657e232833fb17f82e579859a634dccb
CatLava <evan.schoening@gmail.com>
2019-12-23 20:50:17 -0800

    added sudo statements to run in Jenkins

--- run_docker.sh
+++ run_docker.sh
@@ -1,7 +1,7 @@
 # build docker image
-docker build -t doofus909/udacity:app .
+sudo docker build -t doofus909/udacity:app .
 
-docker image ls
+sudo docker image ls
 
-docker run -p 5000:5000 doofus909/udacity:app
-docker run -it doofus909/udacity:app bash 
+sudo docker run -p 5000:5000 doofus909/udacity:app
+sudo docker run -it doofus909/udacity:app bash 
