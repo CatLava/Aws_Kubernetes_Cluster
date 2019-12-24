c69f280a315c95d17089b721885654f019db4397
CatLava <evan.schoening@gmail.com>
2019-12-23 21:06:33 -0800

    Update run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -4,4 +4,4 @@ sudo docker build -t doofus909/udacity:app .
 sudo docker image ls
 
 sudo docker run -p 5000:5000 doofus909/udacity:app
-sudo docker run -it doofus909/udacity:app bash 
+# sudo docker run -it doofus909/udacity:app bash 
